0b6f426f27650c6847bdb21ebb935ceb5fcac3c5
Om Patel <ompatelprogm@gmail.com>
2025-10-24 17:54:20 +0530

    Completed Directory.cpp

---
+++ src/Directory.cpp
@@ -0,0 +1,24 @@
+#include "Directory.h"
+#include <iostream>
+
+
+
+Directory::~Directory(){
+    for(auto const& [name, nodePtr] : children){
+        delete nodePtr;
+    }
+}
+
+void Directory::printInfo() const{
+    std::cout << "drw- " << name;
+}
+
+FileSystemNode* Directory::findChild(const std::string& name){
+    auto it = children.find(name);
+    if (it != children.end()){
+        return it->second;
+    }
+
+    return nullptr;
+}
+
